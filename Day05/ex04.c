1ca74adaf4a4e83f1096200b74b6d3b1481debfa
JoJeongYun <jsgjsy7@naver.com>
2022-01-20 14:29:24 +0900

    최소공배수까지

---
+++ Day05/ex04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+
+// 여기 더 채워야함!
+
+printf("\n");
+
+// 1부터 10까지의 합계
+int sum = 0;
+for(int i = 1; i <= 10 ; i++ ){
+    sum += i;
+}
+printf("합계 : %d\n", sum);
+
+return 0;
+
+
+}
